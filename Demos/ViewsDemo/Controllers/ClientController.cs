4f40a7754931f92e4ba6b441655a25cfd0f52c01
Julien Adam <julien.adam@gmail.com>
2022-11-17 15:56:56 +0100

    Added model binding demo

---
+++ Demos/ViewsDemo/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            var client = new Client
+            {
+                Nom = "Morris",
+                Prenom = "Jeanne",
+                DateNaissance = new DateTime(1975, 11, 5),
+                Societe = "ACME Inc.",
+                Email = "jeanne@acme.inc"
+            };
+            return View(client);
+        }
+    }
+}
---
+++ Demos/ViewsDemo/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewsDemo.Models
+{
+    public class Client
+    {
+        public string Societe { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime? DateNaissance { get; set; }
+        public string Email { get; set; }
+    }
+}
