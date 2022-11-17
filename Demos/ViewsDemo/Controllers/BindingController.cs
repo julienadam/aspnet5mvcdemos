66402834e023ff713874c1e05b1dcecb73015eb2
Julien Adam <julien.adam@gmail.com>
2022-11-17 17:10:49 +0100

    Added model binding for controller

---
+++ Demos/ViewsDemo/Controllers/BindingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers
+{
+    public class BindingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Input(FormCollection coll)
+        {
+            var result =
+                $"From request Form : {Request.Form["Nom"]}" +
+                $"From request Params : {Request.Params["Nom"]}" +
+                $"From FormCollection : {coll["Nom"]}";
+            return Content(result);
+        }
+
+        [HttpPost]
+        public ActionResult Bind(Client c)
+        {
+            var result =
+                $"From bound model {c.Nom}";
+            return Content(result);
+        }
+    }
+}
