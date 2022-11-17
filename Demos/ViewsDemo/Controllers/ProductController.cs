58d9d06eadb8ae127d67c0d5a5dcaeca2c327bc8
Julien Adam <julien.adam@gmail.com>
2022-11-17 17:25:04 +0100

    Binding validation

---
+++ Demos/ViewsDemo/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using ViewsDemo.Models;
+
+namespace ViewsDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Success");
+            }
+
+            return View(product);
+        }
+    }
+}
---
+++ Demos/ViewsDemo/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViewsDemo.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Name your product")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.1, 100_000)]
+        public decimal Price { get; set; }
+    }
+}
