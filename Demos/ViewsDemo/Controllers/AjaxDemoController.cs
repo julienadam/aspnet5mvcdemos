4a4236a0f7a9b89bb02965ddf2c873c76343a67a
Julien Adam <julien.adam@gmail.com>
2022-11-18 12:21:42 +0100

    Added AJAX demo

---
+++ Demos/ViewsDemo/Controllers/AjaxDemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsDemo.Controllers
+{
+    public class AjaxDemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Work()
+        {
+            // Action longue
+            Thread.Sleep(5000);
+            return Content("Finished!");
+        }
+    }
+}
