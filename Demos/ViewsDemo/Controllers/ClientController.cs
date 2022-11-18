ebc03926efc11e079f8e0afb071af2f2d47f83c2
Julien Adam <julien.adam@gmail.com>
2022-11-18 11:28:01 +0100

    List binding demo

--- Demos/ViewsDemo/Controllers/ClientController.cs
+++ Demos/ViewsDemo/Controllers/ClientController.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bogus;
 using ViewsDemo.Models;
 
 namespace ViewsDemo.Controllers
 {
     public class ClientController : Controller
     {
+        private readonly Faker<Client> clientFaker = new Faker<Client>()
+            .RuleFor(c => c.Nom, f => f.Person.LastName)
+            .RuleFor(c => c.Prenom, f => f.Person.FirstName)
+            .RuleFor(c => c.Societe, f => f.Company.CompanyName())
+            .RuleFor(c => c.DateNaissance, f => f.Person.DateOfBirth)
+            .RuleFor(c => c.Email, f => f.Person.Email);
+        
+
         // GET: Client
         public ActionResult Index()
         {
-            var client = new Client
-            {
-                Nom = "Morris",
-                Prenom = "Jeanne",
-                DateNaissance = new DateTime(1975, 11, 5),
-                Societe = "ACME Inc.",
-                Email = "jeanne@acme.inc"
-            };
-            return View(client);
+            return View(clientFaker.Generate());
+        }
+
+        public ActionResult List()
+        {
+            return View(clientFaker.GenerateLazy(25));
         }
     }
 }
