9c1400e184d4f879451c21898cc993e5519a1e7a
Julien Adam <julien.adam@gmail.com>
2022-11-18 11:54:43 +0100

    Added filter demo

---
+++ Demos/FilterDemo/Plumbing/CreateMinidumpOnException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilterDemo.Plumbing
+{
+    public class CreateMinidumpOnExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext.ExceptionHandled)
+            {
+                return;
+            }
+
+            var dumpFileName = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.dmp");
+            MiniDumper.Write(dumpFileName);
+
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new ViewResult()
+            {
+                ViewName = "Error",
+                ViewBag = { dumpFileName = dumpFileName }
+            };
+        }
+    }
+}
